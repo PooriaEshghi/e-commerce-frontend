90324bc6d107aa9700ba4313c122258ed236e8cd
PooriaEshghi <pooriaeshghi@gmail.com>
2023-10-12 16:31:39 +0200

    Refactoring code

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 import { ActivatedRoute } from '@angular/router';
@@ -8,36 +8,40 @@ import { CartItem } from 'src/app/common/cart-item';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css'],
+  styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+
   product: Product = new Product();
-  productService = inject(ProductService);
-  cartService = inject(CartService);
-  route = inject(ActivatedRoute);
-  // constructor(private productService: ProductService,
-  //   private cartService: CartService,
-  //             private route: ActivatedRoute) { }
+
+  constructor(private productService: ProductService,
+              private cartService: CartService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(() => {
       this.handleProductDetails();
-    });
+    })
   }
 
   handleProductDetails() {
-    // get the "id" param string. convert string to a number using the "+" symbol
-    const theProductId: number = +this.route.snapshot.paramMap.get('id')!;
 
-    this.productService.getProduct(theProductId).subscribe((data) => {
-      this.product = data;
-    });
+    // get the "id" param string. convert string to a number using the "+" symbol
+    const theProductId: number = +this.route.snapshot.paramMap.get('id');
+
+    this.productService.getProduct(theProductId).subscribe(
+      data => {
+        this.product = data;
+      }
+    )
   }
+
   addToCart() {
-    console.log(
-      `Adding to cart: ${this.product.name},${this.product.unitPrice}`
-    );
+
+    console.log(`Adding to cart: ${this.product.name}, ${this.product.unitPrice}`);
     const theCartItem = new CartItem(this.product);
     this.cartService.addToCart(theCartItem);
+    
   }
+
 }
